8da214ff0f962a9ac5d2cddbeb591df22b08c621
enigmaDevelopments <enigmarezac@gmail.com>
2025-04-02 13:44:03 -0400

    site sources

---
+++ Hash.cs
@@ -0,0 +1,13 @@
+﻿namespace OpenCryptograph
+{
+    
+    public class Hash
+    {
+       
+    }
+}
+// SHA3 infomation sorces:
+// https://keccak.team/keccak_specs_summary.html
+// https://github.com/XKCP/XKCP/blob/master/Standalone/CompactFIPS202/Python/CompactFIPS202.py
+// https://github.com/XKCP/XKCP/blob/master/Standalone/CompactFIPS202/C/Keccak-readable-and-compact.c
+// https://en.wikipedia.org/wiki/SHA-3
