0de9824973bcf3aca91cf16201799e817a9a9d08
enigmaDevelopments <enigmarezac@gmail.com>
2025-04-02 23:45:37 -0400

    Absorb mad absorbing for Keccak

--- Hash.cs
+++ Hash.cs
@@ -1,16 +1,31 @@
 ﻿using System.Diagnostics;
+using System.Formats.Asn1;
 
 namespace OpenCryptograph
 {
     public static class Hash
     {
-       private static byte[] Keccak(int rate,int capacity, byte[] inputBytes, byte delimitedSuffix, int outputByteLen)
+       private static byte[] Keccak(int rate, int capacity, byte[] input, byte delimitedSuffix, int outputByteLen)
         {
             Debug.Assert(((rate + capacity) != 1600) || ((rate % 8) != 0));
             List<byte> output = new List<byte>();
             byte[] state = new byte[200];
+
             int blockSize = 0;
-            int inputOffset = 0;
+            #region Absorb
+            for (int i = input.Length; i < input.Length; i += blockSize)
+            {
+                blockSize = Math.Min(rate, i-input.Length);
+                for (int j = 0; j<blockSize; j++)
+                    state[j] ^= input[j];
+                i += blockSize;
+                if (i == input.Length)
+                {
+                    // state = KeccakF1600(state); // TO IMPLMENT
+                    blockSize = 0;
+                }
+            }
+            #endregion
         }
     }
 }
