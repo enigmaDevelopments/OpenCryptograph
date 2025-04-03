068452f2f3ac40d2df870a36f205c378133e8d70
enigmaDevelopments <enigmarezac@gmail.com>
2025-04-03 13:20:08 -0400

    added squeezing functionality to Keccak

--- Hash.cs
+++ Hash.cs
@@ -5,27 +5,37 @@ namespace OpenCryptograph
 {
     public static class Hash
     {
-       private static byte[] Keccak(int rate, int capacity, byte[] input, byte delimitedSuffix, int outputByteLen)
+       private static byte[] Keccak(int rate, int capacity, byte[] input, byte delimitedSuffix, int outputLength)
         {
-            Debug.Assert(((rate + capacity) != 1600) || ((rate % 8) != 0));
+            Debug.Assert(((rate*8 + capacity) != 1600));
             List<byte> output = new List<byte>();
             byte[] state = new byte[200];
 
             int blockSize = 0;
             #region Absorb
-            for (int i = input.Length; i < input.Length; i += blockSize)
+            for (int i = input.Length; i < input.Length;)
             {
                 blockSize = Math.Min(rate, i-input.Length);
                 for (int j = 0; j<blockSize; j++)
                     state[j] ^= input[j];
                 i += blockSize;
-                if (i == input.Length)
+                if (rate == blockSize)
                 {
                     // state = KeccakF1600(state); // TO IMPLMENT
                     blockSize = 0;
                 }
             }
             #endregion
+
+            #region squeeze
+            for (int i = 0; i < outputLength; i += blockSize)
+            {
+                // state = KeccakF1600(state); // TO IMPLMENT
+                blockSize = Math.Min(rate, i - outputLength);
+                output.AddRange(state.Take(blockSize));
+            }
+            #endregion
+            return output.ToArray();
         }
     }
 }
