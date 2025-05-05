bbf2b4ea96737c29599d9a3a369ac631a192b315
enigmaDevelopments <enigmarezac@gmail.com>
2025-05-05 19:40:35 -0400

    added messaging to nav bar

--- Website/Controllers/HomeController.cs
+++ Website/Controllers/HomeController.cs
@@ -22,6 +22,10 @@ namespace Website.Controllers
         {
             return View();
         }
+        public IActionResult Message()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
