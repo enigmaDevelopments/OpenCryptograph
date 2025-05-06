4e7b5b067c24e9a98fcf22ce8446b64af1e02e37
enigmaDevelopments <enigmarezac@gmail.com>
2025-05-05 22:07:25 -0400

    make custom datatype for user info

--- Website/Controllers/HomeController.cs
+++ Website/Controllers/HomeController.cs
@@ -7,9 +7,20 @@ using System.Collections.Generic;
 
 namespace Website.Controllers
 {
+    class Info
+    {
+        public readonly BigInteger password;
+        public readonly BigInteger publicKey;
+        public List<BigInteger> messages = new List<BigInteger>(); 
+        public Info(BigInteger password, BigInteger publicKey)
+        {
+            this.password = password;
+            this.publicKey = publicKey;
+        }
+    }
     public class HomeController : Controller
     {
-        Dictionary<BigInteger, BigInteger[]> users = new Dictionary<BigInteger, BigInteger[]>();
+        Dictionary<BigInteger, Info> users = new Dictionary<BigInteger, Info>();
         private readonly ILogger<HomeController> _logger;
         
         public HomeController(ILogger<HomeController> logger)
@@ -18,13 +29,13 @@ namespace Website.Controllers
         }
         public IActionResult SignIn(string username, string password)
         {
-            BigInteger usernameHash = Hash.Shake128(username + password, 2048);
+            BigInteger usernameHash = Hash.Shake128(username, 2048);
             BigInteger passwordHash = Hash.Shake128(username + password, 2048);
             if (!users.ContainsKey(usernameHash)) {
                 Key key = new Key(Hash.Shake128(username + password + username, 1024));
-                users.Add(usernameHash, new BigInteger[] {passwordHash, key.publicKey });
+                users.Add(usernameHash, new Info(passwordHash, key.publicKey));
             }
-                return View("Index");
+            return View("Index");
         }
         public IActionResult Send(string username, string message)
         {
