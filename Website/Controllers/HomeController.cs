dcc3391a2fdbbe098e882b96a3893c8be1aa44f4
enigmaDevelopments <enigmarezac@gmail.com>
2025-05-05 21:42:00 -0400

    save hashed usernames and passwords

--- Website/Controllers/HomeController.cs
+++ Website/Controllers/HomeController.cs
@@ -1,24 +1,30 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 using Website.Models;
+using OpenCryptograph;
 using System.Numerics;
+using System.Collections.Generic;
 
 namespace Website.Controllers
 {
     public class HomeController : Controller
     {
+        Dictionary<BigInteger, BigInteger[]> users = new Dictionary<BigInteger, BigInteger[]>();
         private readonly ILogger<HomeController> _logger;
-
+        
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
         }
         public IActionResult SignIn(string username, string password)
         {
-            Console.WriteLine(username);
-            Console.WriteLine(password);
-            
-            return View("Index");
+            BigInteger usernameHash = Hash.Shake128(username + password, 2048);
+            BigInteger passwordHash = Hash.Shake128(username + password, 2048);
+            if (!users.ContainsKey(usernameHash)) {
+                Key key = new Key(Hash.Shake128(username + password + username, 1024));
+                users.Add(usernameHash, new BigInteger[] {passwordHash, key.publicKey });
+            }
+                return View("Index");
         }
         public IActionResult Send(string username, string message)
         {
